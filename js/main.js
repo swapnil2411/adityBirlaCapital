eea9b533ddd04e59b34627dcfaeb01cb8b81e174
swapnil2411 <67681257+swapnil2411@users.noreply.github.com>
2022-05-23 16:19:23 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,9 +3,13 @@
 $('.hamburger').on('click', function(){
     $('.menu_container .menu_links').addClass('show_menu_links');
     $('.overlay').addClass('show_overlay');
+    $('.hamburger').css('display','none');
+    $('body').css('overflow-y','hidden');
 })
 
 $('.closeLink span').on('click', function(){
     $('.menu_container .menu_links').removeClass('show_menu_links');
     $('.overlay').removeClass('show_overlay');
+    $('.hamburger').css('display','block');
+    $('body').css('overflow-y','auto');
 })
